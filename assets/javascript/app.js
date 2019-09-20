438067f1a147d4a00d6cce8328ce522fba9ccca9
Jorden Ragels <jordenragels3@gmail.com>
2019-09-19 19:03:49 -0700

    changes the background of the jumbotron

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -61,7 +61,7 @@ let marker = new google.maps.Marker({
 fetch(`https://api.foursquare.com/v2/venues/explore?client_id=R3BQG1QOQ5EGU1J40COJNGJ35QB2ZRQBZ3RZU33HGFBCEKLK&client_secret=USPSYAGMLHKZJLEUWVN02IVMVWSLOMWUJDJAIKV22O5YZHHY&v=20180323&limit=13&ll=32.2226,-110.97478&query=venues`)
    .then(response => response.json())
    .then(data =>{
-console.log(data.response.groups[0].items[0].venue.name, data.response.groups[0].items[0].venue.location.lat, data.response.groups[0].items[0].venue.location.lng);
+console.log(data.response);
    });
 
 
